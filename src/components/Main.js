b04839c0d9a0379f92f53904cba7efc6e7bcfafd
Giancarlo Vigneri <vignericodes@gmail.com>
2023-07-12 13:03:47 -0700

    WIP rf start of List/Watched Box components

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ListBox from './ListBox';
+import WatchedBox from './WatchedBox';
+
+function Main() {
+	return (
+		<>
+			<main className='main'>
+				<ListBox />
+				<WatchedBox />
+			</main>
+		</>
+	);
+}
+export default Main;
