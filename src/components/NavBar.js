629b6d2fc89c498b707e50de28056afe2959f62d
Giancarlo Vigneri <vignericodes@gmail.com>
2023-07-12 13:02:47 -0700

    WIP rf NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
+
+import Search from './Search';
+import Logo from './Logo';
+import NumResults from './NumResults';
 
 function NavBar() {
-	const [query, setQuery] = useState('');
-
 	return (
 		<>
 			<nav className='nav-bar'>
-				<div className='logo'>
-					<span role='img'>🍿</span>
-					<h1>usePopcorn</h1>
-				</div>
-				<input
-					className='search'
-					type='text'
-					placeholder='Search movies...'
-					value={query}
-					onChange={(e) => setQuery(e.target.value)}
-				/>
-				<p className='num-results'>
-					Found <strong>X</strong> results
-				</p>
+				<Logo />
+				<Search />
+				<NumResults />
 			</nav>
 		</>
 	);
---
+++ src/components/NumResults.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NumResults() {
+	return (
+		<p className='num-results'>
+			Found <strong>X</strong> results
+		</p>
+	);
+}
+
+export default NumResults;
