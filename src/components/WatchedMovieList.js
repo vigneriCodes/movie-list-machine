c9773c6bdbf6855e37edfd3f4be5a23733c386ab
Giancarlo Vigneri <vignericodes@gmail.com>
2023-07-18 11:13:41 -0700

    add WatchedMovieList

---
+++ src/components/WatchedMovieList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import WatchedMovie from './WatchedMovie';
+
+function WatchedMovieList({ watched }) {
+	return (
+		<ul className='list'>
+			{watched.map((movie) => (
+				<WatchedMovie movie={movie} key={movie.imdbID} />
+			))}
+		</ul>
+	);
+}
+
+export default WatchedMovieList;
