976fc7c7713f81d1aedc7c2deade4f67e8b2c458
Giancarlo Vigneri <vignericodes@gmail.com>
2023-07-20 16:14:20 -0700

    WIP rf to work on StarRating

--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
+// import './index.css';
+// import App from './components/App';
+
+import StarRating from './components/StarRating';
+
+function Test() {
+	const [movieRating, setMovieRating] = useState(0);
+
+	return (
+		<div>
+			<StarRating color='blue' maxRating={10} onSetRating={setMovieRating} />
+			<p>This movie was rated {movieRating} stars</p>
+		</div>
+	);
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<App />
+		{/* <App/> */}
+		<StarRating
+			maxRating={5}
+			messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+		/>
+		<StarRating
+			maxRating={10}
+			size={24}
+			color='green'
+			className='test'
+			defaultRating={3}
+		/>
+		<Test />
 	</React.StrictMode>
 );
