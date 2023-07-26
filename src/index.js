91f828d2cc261a1610197e975eac32069d2279e4
Giancarlo Vigneri <vignericodes@gmail.com>
2023-07-26 12:41:50 -0700

    create oldVersion branch

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './components/App';
+import './index.css';
+import App from './components/App';
 
 import StarRating from './components/StarRating';
 
@@ -19,7 +19,7 @@ function Test() {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		{/* <App/> */}
+		<App />
 		<StarRating
 			maxRating={5}
 			messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
