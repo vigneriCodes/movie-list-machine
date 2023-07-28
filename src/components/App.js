ed032ec4d98c1451f3b96d60eccf290afb8bda7f
Giancarlo Vigneri <vignericodes@gmail.com>
2023-07-28 14:28:32 -0700

    add basic error handling

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,7 @@ import MovieList from './MovieList';
 import WatchedMovieList from './WatchedMovieList';
 import WatchedSummary from './WatchedSummary';
 import Loader from './Loader';
+import ErrorMessage from './ErrorMessage';
 
 const tempMovieData = [
 	{
@@ -63,18 +64,33 @@ export default function App() {
 	const [movies, setMovies] = useState([]);
 	const [watched, setWatched] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState('');
 
 	const query = 'summer';
 
 	useEffect(() => {
 		async function fetchMovies() {
-			setIsLoading(true);
-			const res = await fetch(
-				`http://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`
-			);
-			const data = await res.json();
-			setMovies(data.Search);
-			setIsLoading(false);
+			try {
+				setIsLoading(true);
+				const res = await fetch(
+					`http://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`
+				);
+
+				if (!res.ok)
+					throw new Error('Something went wrong with fetching movies');
+
+				const data = await res.json();
+
+				if (data.Response === 'False') throw new Error('Movie not found');
+
+				setMovies(data.Search);
+				setIsLoading(false);
+			} catch (err) {
+				console.error(err.message);
+				setError(err.message);
+			} finally {
+				setIsLoading(false);
+			}
 		}
 		fetchMovies();
 	}, []);
@@ -91,7 +107,11 @@ export default function App() {
 			</NavBar>
 
 			<Main>
-				<Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
+				<Box>
+					{isLoading && <Loader />}
+					{!isLoading && !error && <MovieList movies={movies} />}
+					{error && <ErrorMessage message={error} />}
+				</Box>
 
 				<Box>
 					<WatchedSummary watched={watched} />
---
+++ src/components/ErrorMessage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ErrorMessage({ message }) {
+	return <p className='error'>--{message}--</p>;
+}
+
+export default ErrorMessage;
