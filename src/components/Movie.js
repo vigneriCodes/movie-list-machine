7b4cfeaf9944c78c4b0601e7f301e44e79d1f83c
Giancarlo Vigneri <vignericodes@gmail.com>
2023-07-28 16:14:51 -0700

    rf add onClick and handler prop/function

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-function Movie({ movie }) {
+function Movie({ movie, handleSelectedMovie }) {
 	return (
-		<li>
+		<li onClick={() => handleSelectedMovie(movie.imdbID)}>
 			<img src={movie.Poster} alt={`${movie.Title} poster`} />
 			<h3>{movie.Title}</h3>
 			<div>
