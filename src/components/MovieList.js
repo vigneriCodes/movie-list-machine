4fb9c6a23f90daaaa432285ce2dca73bb687a00d
Giancarlo Vigneri <vignericodes@gmail.com>
2023-07-28 16:16:38 -0700

    rf prop drill handleSelectedMovie function

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 import Movie from './Movie';
 
-function MovieList({ movies }) {
+function MovieList({ movies, handleSelectMovie, selectedId }) {
 	return (
 		<ul className='list list-movies'>
 			{movies?.map((movie) => (
-				<Movie movie={movie} key={movie.imdbID} />
+				<Movie
+					movie={movie}
+					key={movie.imdbID}
+					handleSelectedMovie={handleSelectMovie}
+				/>
 			))}
 		</ul>
 	);
