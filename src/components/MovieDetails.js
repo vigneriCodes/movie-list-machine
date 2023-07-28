a148083b371cec712ee1912aea5c925e8b91ff44
Giancarlo Vigneri <vignericodes@gmail.com>
2023-07-28 16:18:18 -0700

    WIP MovieDetails component

---
+++ src/components/MovieDetails.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react';
+
+import StarRating from './StarRating';
+import Loader from './Loader';
+
+const API_KEY = process.env.REACT_APP_OMDB_API_KEY;
+
+function MovieDetails({ selectedId, handleCloseMovie }) {
+	const [movie, setMovie] = useState({});
+	const [isLoading, setIsLoading] = useState(false);
+
+	const {
+		Title: title,
+		Poster: poster,
+		Runtime: runtime,
+		Plot: plot,
+		Released: released,
+		Actors: actors,
+		Director: director,
+		Genre: genre,
+		imdbRating,
+	} = movie;
+
+	useEffect(() => {
+		async function getMovieDetails() {
+			setIsLoading(true);
+			const res = await fetch(
+				`http://www.omdbapi.com/?apikey=${API_KEY}&i=${selectedId}`
+			);
+			const data = await res.json();
+			setMovie(data);
+			setIsLoading(false);
+		}
+		getMovieDetails();
+	}, [selectedId]);
+	return (
+		<div className='details'>
+			{isLoading ? (
+				<Loader />
+			) : (
+				<>
+					<header>
+						<button className='btn-back' onClick={handleCloseMovie}>
+							&larr;
+						</button>
+						<img src={poster} alt={`Poster of ${movie}`} />
+						<div className='details-overview'>
+							<h2>{title}</h2>
+							<p>
+								{released} &bull; {runtime}
+							</p>
+							<p>{genre}</p>
+							<p>
+								<span>⭐</span>
+								{imdbRating} IMDb rating
+							</p>
+						</div>
+					</header>
+					<section>
+						<div className='rating'>
+							<StarRating maxRating={10} size={24} />
+						</div>
+						<p>
+							<em>{plot}</em>
+						</p>
+						<p>Starring {actors}</p>
+						<p>Directed by {director}</p>
+					</section>
+				</>
+			)}
+		</div>
+	);
+}
+
+export default MovieDetails;
