ed84852ca11364ecfea7a3eafa5a883fb7886ed1
Giancarlo Vigneri <vignericodes@gmail.com>
2023-08-11 15:06:02 -0700

    rm tempWatchedData (placeholder data)

--- src/components/App.js
+++ src/components/App.js
@@ -12,29 +12,6 @@ import Loader from './Loader';
 import ErrorMessage from './ErrorMessage';
 import MovieDetails from './MovieDetails';
 
-const tempWatchedData = [
-	{
-		imdbID: 'tt1375666',
-		Title: 'Inception',
-		Year: '2010',
-		Poster:
-			'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
-		runtime: 148,
-		imdbRating: 8.8,
-		userRating: 10,
-	},
-	{
-		imdbID: 'tt0088763',
-		Title: 'Back to the Future',
-		Year: '1985',
-		Poster:
-			'https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
-		runtime: 116,
-		imdbRating: 8.5,
-		userRating: 9,
-	},
-];
-
 const API_KEY = process.env.REACT_APP_OMDB_API_KEY;
 
 export default function App() {
