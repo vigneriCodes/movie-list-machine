38c6ab1a1845f73a8c7d20720677e1f5588c7d27
Giancarlo Vigneri <vignericodes@gmail.com>
2023-08-12 14:14:43 -0700

    small refactor

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 
 function Search({ setQuery }) {
 	const [searchInput, setSearchInput] = useState('');
+
 	const inputElement = useRef(null);
 
 	useEffect(() => {
